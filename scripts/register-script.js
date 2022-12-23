ab04e78670734ab63e0fecf80a07bc2b86e03fe1
Noshi96 <2spawel2@gmail.com>
2022-12-23 23:55:36 +0100

    IP23JS-5 Add validation for user name, and email

--- scripts/register-script.js
+++ scripts/register-script.js
@@ -33,8 +33,72 @@ class RegisterModel {
       id: self.crypto.randomUUID(),
     };
     console.log({ user });
-    this.users.push(user);
-    this.#commit(this.users);
+    //this.users.push(user);
+    //this.#commit(this.users);
+  }
+
+  validateUserName(name) {
+    const userRegExp = /^[a-zA-Z0-9 \/\\[\]_-]+$/;
+    const isUserNameValid = name.match(userRegExp);
+    const countLetters = str.replace(/[^[a-zA-Z]/g, '').length;
+    const countDigits = str.replace(/[^0-9]/g, '').length;
+    const isDuplicated = this.users.some(({ userName }) => userName === name);
+
+    if (!isUserNameValid) {
+      return 'User name is not valid.';
+    }
+    if (isDuplicated) {
+      return 'Duplicated user name.';
+    }
+    if (countLetters < 5) {
+      return 'Not enough characters.';
+    }
+    if (countDigits < 1) {
+      return 'Not enough digits.';
+    }
+  }
+
+  validateUserEmail(email) {
+    const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const isEmailValid = email.match(emailRegExp);
+
+    if (!isEmailValid) {
+      return 'Wrong email format.';
+    }
+
+    const isPlusInEmail = email.split('').some((char) => char === '+');
+    const firstPartOfEmail = email.split('+').at(0);
+
+    if (isPlusInEmail) {
+      const userWithDuplicatedEmailFirstPart = this.users.filter(
+        ({ userEmail }) => userEmail.split('+').at(0) === firstPartOfEmail
+      );
+      // myEmail+elo@gmail.com and myEmail+elo@wp.pl are not the same
+      if (userWithDuplicatedEmailFirstPart) {
+        const domain = userWithDuplicatedEmailFirstPart.email.split('@').at(1);
+        const duplicatedEmailWithAlias = email.split('@').at(1) === domain;
+        if (duplicatedEmailWithAlias) {
+          return 'Duplicated email with alias.';
+        }
+      }
+    }
+
+    const isDuplicated = this.users.some(
+      ({ userEmail }) => userEmail === email
+    );
+    if (isDuplicated) {
+      return 'Duplicated email.';
+    }
+  }
+
+  #hashUserPassword(password) {
+    let hash = 0;
+    for (let i = 0, len = password.length; i < len; i++) {
+      let chr = password.charCodeAt(i);
+      hash = (hash << 5) - hash + chr;
+      hash |= 0;
+    }
+    return hash;
   }
 }
 
@@ -59,7 +123,11 @@ class RegisterView {
             type="text"
             placeholder="${userNameInputText}"
             name="user-name"
+            required 
+            minlength="6"
+            maxlength="16"
           />
+          <span class="error-user-name" aria-live="polite"></span>
         </div>
         <div class="single-input">
           <label for="user-password">${userPasswordInputLabel}</label>
@@ -68,7 +136,10 @@ class RegisterView {
             type="password"
             placeholder="${userPasswordInputText}"
             name="user-password"
+            required 
+            minlength="6"
           />
+          <span class="error-user-password" aria-live="polite"></span>
         </div>
         <div class="single-input">
           <label for="user-email">${userEmailInputLabel}</label>
@@ -77,7 +148,9 @@ class RegisterView {
             type="email"
             placeholder="${userEmailInputText}"
             name="user-email"
+            required
           />
+          <span class="error-user-email" aria-live="polite"></span>
         </div>
         <div class="single-input">
           <label for="user-confirm-email">${userEmailConfirmationInputLabel}</label>
@@ -86,7 +159,9 @@ class RegisterView {
             type="email"
             placeholder="${userEmailConfirmationInputText}"
             name="user-confirm-email"
+            required
           />
+          <span class="error-user--confirm-email" aria-live="polite"></span>
         </div>
         <button class="register-button">${registerButtonText}</button>
       </form>
@@ -122,13 +197,33 @@ class RegisterView {
 
   createElement(tag, className) {
     const element = document.createElement(tag);
-
     if (className) element.classList.add(className);
 
     return element;
   }
 
   bindAddUser(handler) {
+    // this.form.addEventListener('submit', (event) => {
+    //   event.preventDefault();
+    //   const user = {
+    //     userName: this.#userName,
+    //     userPassword: this.#userPassword,
+    //     userEmail: this.#userEmail,
+    //     userConfirmEmail: this.#userConfirmEmail,
+    //   };
+    //   if (
+    //     this.#userName &&
+    //     this.#userPassword &&
+    //     this.#userEmail &&
+    //     this.#userConfirmEmail
+    //   ) {
+    //     handler(user);
+    //     this.#resetInputs();
+    //   }
+    // });
+  }
+
+  bindValidateUserData(handler) {
     this.form.addEventListener('submit', (event) => {
       event.preventDefault();
       const user = {
@@ -137,15 +232,7 @@ class RegisterView {
         userEmail: this.#userEmail,
         userConfirmEmail: this.#userConfirmEmail,
       };
-      if (
-        this.#userName &&
-        this.#userPassword &&
-        this.#userEmail &&
-        this.#userConfirmEmail
-      ) {
-        handler(user);
-        this.#resetInputs();
-      }
+      handler(user);
     });
   }
 }
@@ -164,10 +251,15 @@ class RegisterController {
     this.view = view;
 
     this.view.bindAddUser(this.handleAddUser);
+    this.view.bindValidateUserData(this.handleValidateUserData);
   }
 
   handleAddUser = (userProp) => {
     this.model.addUser(userProp);
+  };
+
+  handleValidateUserData = (user) => {
+    this.model.validateUserData(user);
   };
 }
 
